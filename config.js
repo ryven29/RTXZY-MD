cd46b19cf4547dff2578efb807a1391461b0fa8e
ryven29 <155292524+ryven29@users.noreply.github.com>
2024-11-19 10:08:53 +0800

    Add files via upload

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
-global.owner = ['62895331520602']  
-global.mods = ['62895331520602'] 
-global.prems = ['62895331520602']
-global.nameowner = 'Tio'
-global.numberowner = '62895331520602' 
-global.mail = 'support@tioprm.eu.org' 
-global.gc = 'https://chat.whatsapp.com/CP5mV2D9IO730HpBgmkuyi'
-global.instagram = 'https://instagram.com/prm2.0'
-global.wm = '© Tio'
+global.owner = ['6282169690832']  
+global.mods = ['6282169690832'] 
+global.prems = ['6282169690832']
+global.nameowner = 'Ryven'
+global.numberowner = '6282169690832' 
+global.mail = 'ryvenz126@gmail.com' 
+global.gc = 'https://chat.whatsapp.com/GGxMAsngYDFDczrIlbl3wd'
+global.instagram = 'https://instagram.com/fikrinrirham_'
+global.wm = '© Glyphic'
 global.wait = '_*Tunggu sedang di proses...*_'
 global.eror = '_*Server Error*_'
 global.stiker_wait = '*⫹⫺ Stiker sedang dibuat...*'
@@ -17,11 +17,11 @@ global.maxwarn = '3' // Peringatan maksimum
 global.antiporn = true // Auto delete pesan porno (bot harus admin)
 
 //INI WAJIB DI ISI!//
-global.btc = 'YOUR_APIKEY_HERE' 
+global.btc = 'd6geQEKP' 
 //Daftar terlebih dahulu https://api.botcahx.eu.org
 
 //INI OPTIONAL BOLEH DI ISI BOLEH JUGA ENGGA//
-global.lann = 'YOUR_APIKEY_HERE'
+global.lann = 'YeRdinCvd'
 //Daftar https://api.betabotz.eu.org 
 
 //jangan diganti!
@@ -31,7 +31,7 @@ global.APIs = {
 
 //ini tidak di isi juga tidak apa-apa
 global.APIKeys = { 
-  'https://api.botcahx.eu.org': 'APIKEY' 
+  'https://api.botcahx.eu.org': 'eRdinCvd' 
 }
 
 let fs = require('fs')
